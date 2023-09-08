f056262ba1a9919614873ca4e6abfb5453766aba
SyrymAbdikhan <syrym.abdikhan@gmail.com>
2023-09-09 01:10:29 +0600

    removed deadlines formatter

--- bot/db/models.py
+++ bot/db/models.py
@@ -3,19 +3,4 @@ from bot.db.base import Base
 
 from sqlalchemy import Column, BigInteger, Boolean, select
 
-
-class Formatter(Base):
-    __tablename__ = 'formatter'
-
-    chat_id = Column(BigInteger, unique=True, primary_key=True)
-    auto_format = Column(Boolean, default=True)
-    auto_delete = Column(Boolean, default=True)
-
-    @classmethod
-    async def find(self, db_session, chat_id):
-        sql = select(self).where(self.chat_id == chat_id)
-        result = await db_session.execute(sql)
-        return result.scalars().first()
-
-    def __str__(self) -> str:
-        return f'<Formatter:{self.chat_id}>'
+# no need of db for now
--- bot/handlers/common.py
+++ bot/handlers/common.py
@@ -2,36 +2,11 @@
 import logging
 import re
 
-from telethon import events, Button
-from bot.db.models import Formatter
+from telethon import events
 
 from bot.loader import bot
 from bot.utils.funcs import *
 from bot.utils.decorators import logger
-
-
-async def get_formatter(chat_id):
-    async with bot.db() as db_session:
-        formatter = await Formatter.find(db_session, chat_id)
-        if formatter is None:
-            formatter = Formatter(chat_id=chat_id)
-            await formatter.save(db_session)
-    
-    return formatter
-
-
-async def get_kb(chat_id):
-    formatter = await get_formatter(chat_id)
-    return [
-        Button.inline(
-            f'Auto format: {"✅" if formatter.auto_format else "❌"}',
-            'auto_format'
-        ),
-        Button.inline(
-            f'Auto delete: {"✅" if formatter.auto_delete else "❌"}',
-            'auto_delete'
-        )
-    ]
 
 
 @bot.on(events.NewMessage(pattern=r'/start'))
@@ -46,55 +21,9 @@ async def cmd_start(event):
 async def cmd_help(event):
     await send_message(event, 'Here is the bot commands:\n\n'
                               '/all - mention all members of the group\n'
-                              '/formatter - deadline formatter settings\n'
+                              '/deadlines - shows all deadlines\n'
                               '/help - more information about bot\n\n'
                               'Developer contacts: @Honey_Niisan')
-
-
-@bot.on(events.NewMessage(pattern=r'🔥 Ближайшие дедлайны на 60 дней:'))
-@logger
-async def auto_formatter(event):
-    formatter = await get_formatter(event.chat_id)
-    if not formatter.auto_format:
-        return
-
-    text = event.raw_text
-    new_text = re.sub("\n\d+\. ", "\n\n===========\n", text)
-    new_text = re.sub(" \| ", "\n", new_text)
-
-    await send_message(event, new_text)
-    
-    if formatter.auto_delete:
-        await event.delete()
-
-
-@bot.on(events.NewMessage(pattern=r'/formatter'))
-@logger
-async def cmd_formatter_settings(event):
-    keyboard = await get_kb(event.chat_id)
-    await send_message(event, '⚙️ settings', keyboard=keyboard)
-
-
-@bot.on(events.CallbackQuery)
-@logger
-async def formatter_settings_handler(event):
-    data = bytes.decode(event.data, 'utf-8')
-
-    formatter = await get_formatter(event.chat_id)
-    async with bot.db() as db_session:
-        if data == 'auto_format':
-            await formatter.update(
-                db_session,
-                auto_format=not formatter.auto_format
-            )
-        elif data == 'auto_delete':
-            await formatter.update(
-                db_session,
-                auto_delete=not formatter.auto_delete
-            )
-
-    keyboard = await get_kb(event.chat_id)
-    await bot.edit_message(event.chat_id, event.original_update.msg_id, buttons=keyboard)
 
 
 @bot.on(events.ChatAction())
