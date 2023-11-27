9c9793e7601224543d8195cf7ec52ebaf1c2b780
SyrymAbdikhan <syrym.abdikhan@gmail.com>
2023-11-27 14:02:56 +0600

    added queue

--- bot/handlers/user_cmd.py
+++ bot/handlers/user_cmd.py
@@ -2,9 +2,9 @@
 from telethon import events
 
 from bot.loader import bot
-from bot.utils.funcs import (send_message, get_buttons,
-                             get_member_ids, make_tag, groupby,
-                             get_db)
+from bot.utils.funcs import (send_message, get_web_buttons, get_user_tag,
+                             get_member_ids, get_user_mention, groupby,
+                             get_db, get_Q_buttons, rows_to_buttons)
 from bot.utils.decorators import logger, is_group, type_action
 from bot.db.models import ChatSettings
 
@@ -15,7 +15,7 @@ from bot.db.models import ChatSettings
 @type_action
 async def cmd_settoken(event):
     db_session = await get_db(bot.db)
-    
+
     args = event.text.split()
     if len(args) < 2:
         await send_message(event, '❗️Too less arguments!\n/settoken <token>', reply=True)
@@ -23,16 +23,16 @@ async def cmd_settoken(event):
     elif len(args) > 2:
         await send_message(event, '❗️Too many arguments!\n/settoken <token>', reply=True)
         return await db_session.close()
-    
+
     token = args[1]
     chat = await ChatSettings.find(db_session, event.chat_id)
     if chat is None:
         chat = ChatSettings(chat_id=event.chat_id, moodle_token=token)
     else:
         chat.moodle_token = token
-    
+
     await chat.save(db_session)
-    
+
     await send_message(event, '✅ Successfully set token! New commands available now', reply=True)
     await db_session.close()
 
@@ -43,7 +43,7 @@ async def cmd_settoken(event):
 @type_action
 async def cmd_all(event):
     ids = await get_member_ids(event.chat_id)
-    mentions = [make_tag(_id) for _id in ids]
+    mentions = [get_user_mention(_id) for _id in ids]
 
     texts = groupby(mentions, bot.config.msg_size, '')
     texts = list(map(lambda el: ''.join(el), texts))
@@ -61,14 +61,41 @@ async def cmd_deadlines(event):
     db_session = await get_db(bot.db)
     chat = await ChatSettings.find(db_session, event.chat_id)
     await db_session.close()
-    
+
     if chat is None:
         return await send_message(event, '❗️Please set token (/settoken) to use this command', reply=True)
-    
+
     await send_message(
         event,
         'Here is the list of deadlines\nClick to Open 👇',
         reply=True,
-        buttons=get_buttons(bot.config.webapp_url, chat.moodle_token)
+        buttons=get_web_buttons(bot.config.webapp_url, chat.moodle_token)
     )
+
+
+@bot.on(events.NewMessage(pattern='^/newQ'))
+async def create_Q_handler(event: events.NewMessage.Event):
+    text = event.text.split(maxsplit=1)
+    title = text[1] if len(text) > 1 else 'New Queue'
+
+    await event.respond(f'{title}\n\nClick to Join the queue 👇', buttons=get_Q_buttons(add_empty_spaces=3))
+
+
+@bot.on(events.CallbackQuery(pattern='QT[0-9]+'))
+async def join_QTn_callback(query: events.CallbackQuery.Event):
+    return await query.answer('This place is already taken')
+
+
+@bot.on(events.CallbackQuery(data='quit'))
+async def quit_Q_callback(query: events.CallbackQuery.Event):
+    tag = get_user_tag(query.sender)
+    msg = await query.get_message()
+    btn_rows = msg.reply_markup.rows[:-1]
     
+    old_buttons = rows_to_buttons(btn_rows)
+    buttons = rows_to_buttons(btn_rows, tag)
+    if old_buttons == buttons:
+        return await query.answer('You are not in the queue!')
+
+    add = int(buttons[-1][1][:2] == b'QT')
+    await query.edit(msg.message, buttons=get_Q_buttons(buttons, add_empty_spaces=add))
--- bot/utils/funcs.py
+++ bot/utils/funcs.py
@@ -15,10 +15,45 @@ async def send_message(event, text, reply=False, **kwargs):
     return msg
 
 
-def get_buttons(webapp_url, token):
+def get_web_buttons(webapp_url, token):
     return bot.build_reply_markup([
         [Button.url(text='open', url=f'{webapp_url}?startapp={token}')],
     ])
+
+
+def get_Q_buttons(users=None, add_empty_spaces=0):
+    users = users or []
+
+    buttons = [
+        [Button.inline(text, data)]
+        for text, data in users
+    ]
+
+    start_at = len(users)+1
+    buttons += [
+        [Button.inline(f'{start_at+i}. empty', str.encode(f'QE{start_at+i}'))]
+        for i in range(add_empty_spaces)
+    ]
+    buttons += [[Button.inline('Quit', 'quit')]]
+    return bot.build_reply_markup(buttons)
+
+
+def rows_to_buttons(button_rows, tag=None, target=None):
+    tag = tag or ''
+    buttons = []
+    for i, row in enumerate(button_rows):
+        btn = row.buttons[0]
+        if tag in btn.text and tag:
+            buttons.append((f'{i+1}. empty', str.encode(f'QE{i+1}')))
+        elif btn.data == target:
+            buttons.append((f'{i+1}. {tag}', str.encode(f'QT{i+1}')))
+        else:
+            buttons.append((btn.text, btn.data))
+
+    while buttons[-1][1][:2] == b'QE' and len(buttons) > 3:
+        buttons = buttons[:-1]
+
+    return buttons
 
 
 async def get_member_ids(chat_id):
@@ -27,7 +62,15 @@ async def get_member_ids(chat_id):
     return tuple(map(lambda m: m.id, members))
 
 
-def make_tag(_id, display_name='ඞ'):
+def get_user_tag(user):
+    tag = user.first_name
+    if user.username:
+        tag = '@' + user.username
+
+    return tag
+
+
+def get_user_mention(_id, display_name='ඞ'):
     return f'[{display_name}](tg://user?id={_id})'
 
 
